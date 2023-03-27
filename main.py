56fd48b319dad58cfd710134d61d73da43d8b374
Elvin Butcovan <ebutcovan@gmail.com>
2023-03-27 12:46:48 +0100

    updated implementation

--- main.py
+++ main.py
@@ -3,46 +3,6 @@ import json
 from types import SimpleNamespace
 import project as model
 
-#def messageBuilder(exercises):
-#    iter = 0
-#    message = "[{\n"
-#    for exercise in exercises:
-#        muscleType = exercise[3]
-#        exerciseDescription = exercise[0]
-#        reps = exercise[1]
-#        weight = exercise[2]
-#        message += "\"muscleType\": " + "\"" + muscleType + "\",\n"
-#        message += "\"exerciseDescription\": " + "\"" + exerciseDescription + "\",\n"
-#        message += "\"reps\": " + "\"" + str(reps) + "\",\n"
-#        message += "\"weight\": " + "\"" + str(weight) + "\",\n"
-#        message += "},\n"
-#        iter+=1
-#        if(iter != len(exercises)):
-#            message+="{"
-
-#    message += "]"
-#    return message
-
-#class handler(BaseHTTPRequestHandler):
-
-#    def do_POST(self):
-#        self.send_response(200)
-#        self.send_header('Content-type','application/json')
-#        self.end_headers()
-#        content_len = int(self.headers.get('Content-Length'))
-#        post_body = self.rfile.read(content_len).decode("utf-8") # Request body as json string
-#        #print(post_body)
-
-#        args = json.loads(post_body, object_hook=lambda d: SimpleNamespace(**d)) # Convert body into object with addressable fields.
-
-        #repsonse
-#        message = model.run_model(args)
-#        self.wfile.write(bytes(messageBuilder(message), "utf8"))
-
-#if __name__ == "__main__":
-#    with HTTPServer(('', 8000), handler) as server:
-#        server.serve_forever()
-
 def messageBuilder(exercises):
     message = []
 
--- project.py
+++ project.py
@@ -6,40 +6,117 @@ import numpy as np
 import re
 import random
 
-dataset= pd.read_csv('Dataset/weightlifting_721_workouts.csv')
+def clean_exercise_name(exercise_name):
+    # Remove extra spaces before or after parentheses
+    cleaned_name = re.sub(r'\s*\(\s*', ' (', exercise_name)
+    cleaned_name = re.sub(r'\s*\)\s*', ')', cleaned_name)
+    # Replace "Hammer Curl (Dumbbell)" with "Hammer Curl"
+    cleaned_name = cleaned_name.replace("Hammer Curl (Dumbbell)", "Hammer Curl")
 
-# Clean variable names and select first 6 columns
-dataset = (
-    dataset
-    .clean_names()
-    .iloc[:, :6]
-)
+    return cleaned_name
 
-# Title-case exercise_name and workout_name columns, and convert 2nd to 4th columns to factors
-dataset[['exercise_name', 'workout_name']] = (
-    dataset[['exercise_name', 'workout_name']]
-    .apply(lambda x: x.str.replace('.', '', regex=True).str.title())
-)
-dataset[dataset.columns[1:4]] = dataset.iloc[:, 1:4].astype('category')
+def preprocess_dataset():
+    dataset = pd.read_csv('Dataset/weightlifting_721_workouts.csv')
 
-# Convert weight column from lbs to kg
-dataset['weight'] = dataset['weight'].apply(lambda x: x * 0.453592)
+    # Clean variable names and select first 6 columns
+    dataset = (
+        dataset
+        .clean_names()
+        .iloc[:, :6]
+    )
 
-# Round the 'weight' column down to the nearest integer
-dataset['weight'] = dataset['weight'].apply(lambda x: int(x))
+    # Title-case exercise_name and workout_name columns, and convert 2nd to 4th columns to factors
+    dataset[['exercise_name', 'workout_name']] = (
+        dataset[['exercise_name', 'workout_name']]
+        .apply(lambda x: x.str.replace('.', '', regex=True).str.title())
+    )
 
-# Create a new DataFrame containing the counts of each unique value in the 'exercise_name' column
-exercise_counts = pd.DataFrame(dataset['exercise_name'].value_counts())
+    dataset['exercise_name'] = dataset['exercise_name'].apply(clean_exercise_name)
 
-# Remove the column named 'column_to_remove'
-dataset = dataset.drop(columns=['date'])
-dataset = dataset.drop(columns=['set_order'])
+    dataset[dataset.columns[1:4]] = dataset.iloc[:, 1:4].astype('category')
 
-# Save the updated dataset
-dataset.to_csv('dataset.csv', index=False)
+    # Convert weight column from lbs to kg
+    dataset['weight'] = dataset['weight'].apply(lambda x: x * 0.453592)
 
-# Define a regular expression pattern to extract muscle group name from workout_name
-pattern = r'([A-Za-z]+)'
+    # Round the 'weight' column down to the nearest integer
+    dataset['weight'] = dataset['weight'].apply(lambda x: int(x))
+
+    # Create a new DataFrame containing the counts of each unique value in the 'exercise_name' column
+    exercise_counts = pd.DataFrame(dataset['exercise_name'].value_counts())
+
+    # Remove the column named 'column_to_remove'
+    dataset = dataset.drop(columns=['date'])
+    dataset = dataset.drop(columns=['set_order'])
+
+    # Save the updated dataset
+    dataset.to_csv('dataset.csv', index=False)
+
+    # Define a regular expression pattern to extract muscle group name from workout_name
+    pattern = r'([A-Za-z]+)'
+
+    # Apply the extract_muscle_group function to workout_name column
+    dataset['muscle_group'] = dataset['workout_name'].apply(extract_muscle_group)
+
+    # Drop rows where the muscle_group column is None
+    dataset = dataset.dropna(subset=['muscle_group'])
+
+    # Define a lambda function to check if 'tricep' is in the exercise name
+    check_for_tricep = lambda x: 'Triceps' if 'tricep' in x['exercise_name'].lower() else x['muscle_group']
+
+    # Define a lambda function to check if 'bicep' is in the exercise name
+    check_for_bicep = lambda x: 'Biceps' if 'bicep' in x['exercise_name'].lower() else x['muscle_group']
+
+    # Define a lambda function to check if 'dips' is in the exercise name
+    check_for_dips = lambda x: 'Chest' if 'dips' in x['exercise_name'].lower() else x['muscle_group']
+
+    # Define a lambda function to check if 'bench' is in the exercise name
+    check_for_bench = lambda x: 'Chest' if 'bench' in x['exercise_name'].lower() else x['muscle_group']
+
+    # Define a lambda function to check if 'incline press' is in the exercise name
+    check_for_incline = lambda x: 'Chest' if 'incline press' in x['exercise_name'].lower() else x['muscle_group']
+
+    # Define a lambda function to check if 'cable fly' is in the exercise name
+    check_for_fly = lambda x: 'Chest' if 'cable fly' in x['exercise_name'].lower() else x['muscle_group']
+
+    # Define a lambda function to check if 'hammer curl' is in the exercise name
+    check_for_hammer = lambda x: 'Biceps' if 'hammer curl' in x['exercise_name'].lower() else x['muscle_group']
+
+    # Define a lambda function to check if 'military press' is in the exercise name
+    check_for_military = lambda x: 'Shoulders' if 'military press' in x['exercise_name'].lower() else x['muscle_group']
+
+    # Define a lambda function to check if 'lateral raise' is in the exercise name
+    check_for_lateral = lambda x: 'Shoulders' if 'lateral raise' in x['exercise_name'].lower() else x['muscle_group']
+
+    # Define a lambda function to check if 'shoulder' is in the exercise name
+    check_for_shoulder = lambda x: 'Shoulders' if 'shoulder' in x['exercise_name'].lower() else x['muscle_group']
+
+    # Define a lambda function to check if 'skullcrusher' is in the exercise name
+    check_for_skullcrusher = lambda x: 'Triceps' if 'skullcrusher' in x['exercise_name'].lower() else x['muscle_group']
+
+    # Define a lambda function to check if 'overhead press' is in the exercise name
+    check_for_overhead = lambda x: 'Shoulders' if 'overhead press' in x['exercise_name'].lower() else x['muscle_group']
+
+    # Define a lambda function to check if 'rotator cuff work' is in the exercise name
+    check_for_rotator = lambda x: 'Shoulders' if 'rotator cuff work' in x['exercise_name'].lower() else x[
+        'muscle_group']
+
+    # Apply the lambda functions to the muscle_group column
+    dataset = dataset.copy()
+    dataset['muscle_group'] = dataset.apply(check_for_tricep, axis=1)
+    dataset['muscle_group'] = dataset.apply(check_for_bicep, axis=1)
+    dataset['muscle_group'] = dataset.apply(check_for_dips, axis=1)
+    dataset['muscle_group'] = dataset.apply(check_for_bench, axis=1)
+    dataset['muscle_group'] = dataset.apply(check_for_incline, axis=1)
+    dataset['muscle_group'] = dataset.apply(check_for_fly, axis=1)
+    dataset['muscle_group'] = dataset.apply(check_for_hammer, axis=1)
+    dataset['muscle_group'] = dataset.apply(check_for_military, axis=1)
+    dataset['muscle_group'] = dataset.apply(check_for_lateral, axis=1)
+    dataset['muscle_group'] = dataset.apply(check_for_shoulder, axis=1)
+    dataset['muscle_group'] = dataset.apply(check_for_skullcrusher, axis=1)
+    dataset['muscle_group'] = dataset.apply(check_for_overhead, axis=1)
+    dataset['muscle_group'] = dataset.apply(check_for_rotator, axis=1)
+
+    return dataset
 
 def clean_workout_name(workout_name):
     # Replace "shoulder" with "shoulders"
@@ -66,33 +143,7 @@ def extract_muscle_group(workout_name):
     else:
         return None
 
-def do_model_stuff(dataset, apiArgs):
-    # Apply the extract_muscle_group function to workout_name column
-    dataset['muscle_group'] = dataset['workout_name'].apply(extract_muscle_group)
-
-    # Drop rows where the muscle_group column is None
-    dataset = dataset.dropna(subset=['muscle_group'])
-
-    # Define a lambda function to check if 'tricep' is in the exercise name
-    check_for_tricep = lambda x: 'Triceps' if 'tricep' in x['exercise_name'].lower() else x['muscle_group']
-
-    # Define a lambda function to check if 'bicep' is in the exercise name
-    check_for_bicep = lambda x: 'Biceps' if 'bicep' in x['exercise_name'].lower() else x['muscle_group']
-
-    # Apply the lambda function to the muscle_group column
-    dataset = dataset.copy()
-    dataset['muscle_group'] = dataset.apply(check_for_tricep, axis=1)
-
-    # Apply the lambda function to the muscle_group column
-    dataset['muscle_group'] = dataset.apply(check_for_bicep, axis=1)
-
-    # Ask the user for their preferred muscle groups, number of reps, and weight
-    #pref_muscle_groups = input("Enter the muscle groups you want to train, separated by commas: ")
-
-    #for muscle in pref_muscle_groups.split(','):
-    #    preferences[muscle] = {'reps': int(input("Enter the number of reps you prefer for {}: ".format(muscle))),
-    #                           'weight': int(input("Enter the weight you prefer for {}: ".format(muscle)))}
-
+def generate_recommendations(dataset, apiArgs):
     preferences = {}
     muscleArr = []
     for muscleNum in range(len(apiArgs.muscleGroups)):
@@ -126,7 +177,7 @@ def do_model_stuff(dataset, apiArgs):
     similarity_scores = cosine_similarity([user_vector], exercise_vectors)[0]
 
     # Rank the exercises for each muscle group based on their similarity scores and recommend the top 5
-    num_recommendations = 10
+    num_recommendations = 20
     top_exercises_by_muscle = {}
     for muscle in muscleArr:
         muscle_df = exercise_features[exercise_features['muscle_group'] == muscle]
@@ -137,17 +188,22 @@ def do_model_stuff(dataset, apiArgs):
         top_exercises_by_muscle[muscle] = muscle_df[:num_recommendations][
             ['exercise_name', 'reps', 'weight', 'muscle_group']].values.tolist()
 
-    # Randomly select 5 exercises from the top 10 recommended exercises for each muscle group
+    # Randomly select up to 5 unique exercises from the top 20 recommended exercises for each muscle group
     random_recommendations = []
     for muscle, exercises in top_exercises_by_muscle.items():
-        random_recommendations.extend(random.sample(exercises, 5))
+        unique_exercises = []
+        random.shuffle(exercises)  # Randomize the order of exercises
+
+        for exercise in exercises:
+            if exercise[0] not in [ex[0] for ex in unique_exercises]:  # Check if the exercise name is unique
+                unique_exercises.append(exercise)
+                if len(unique_exercises) == 5:  # Stop adding exercises once 5 unique ones have been selected
+                    break
+
+        random_recommendations.extend(unique_exercises)
     return random_recommendations
 
-    # Print the recommended exercises
-    #print("Here are 5 randomly selected exercises from the top {} recommended exercises for each muscle group:".format(
-        #num_recommendations))
-    #for i, exercise in enumerate(random_recommendations):
-        #print("{}. Exercise Name: {}, Reps: {}, Weight: {}, Muscle Group: {}".format(i + 1, exercise[0], exercise[1],
-#do_model_stuff(dataset)                                                              #exercise[2], exercise[3]))
+
 def run_model(apiArgs):
-    return do_model_stuff(dataset, apiArgs)
+    dataset = preprocess_dataset()
+    return generate_recommendations(dataset, apiArgs)
